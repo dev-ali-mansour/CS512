4bfd467ce57f62ff48ba7500816c0c636e405ed7
Ali Mansour <dev.ali.mansour@gmail.com>
2024-11-11 05:29:09 +0200

    refactor: Rename linear_search/implementation.linearSearch() To linear_search().

--- linear_search/implementation.cpp
+++ linear_search/implementation.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int linearSearch(const int arr[], int size, int x);
+int linear_search(const int arr[], int size, int x);
 
 int main() {
     int arr[] = {2, 5, 6, 7, 2, 3, 5, 14, 39};
@@ -10,7 +10,7 @@ int main() {
     cout << "Please enter an integer:";
     cin >> x;
     int size = sizeof(arr) / sizeof(arr[0]);
-    int index = linearSearch(arr, size, x);
+    int index = linear_search(arr, size, x);
     if (index == -1)
         cout << x << " is not found\n";
     else
@@ -18,7 +18,7 @@ int main() {
     return 0;
 }
 
-int linearSearch(const int arr[], int size, int x) {
+int linear_search(const int arr[], int size, int x) {
     int index = -1;
     for (int i = 0; i < size; ++i) {
         if (arr[i] == x) {
