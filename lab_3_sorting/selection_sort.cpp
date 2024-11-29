4941b7dddc79f22a20fad14181654b6c7d2f1abf
Ali Mansour <dev.ali.mansour@gmail.com>
2024-11-29 11:40:23 +0200

    feat: Implement Selection Sort From Lab 2.

--- lab_3_sorting/selection_sort.cpp
+++ lab_3_sorting/selection_sort.cpp
@@ -5,15 +5,17 @@ using namespace std;
 int main() {
     int array[] = {3, 1, 6, 2, 8, 12, 4};
     int size = sizeof(array) / sizeof(array[0]);
-    int temp;
+    int temp, minIndex;
+
     for (int i = 0; i < size; ++i) {
+        minIndex = i;
         for (int j = i + 1; j < size; ++j) {
-            if (array[i] > array[j]) {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
+            if (array[minIndex] > array[j])
+                minIndex = j;
         }
+        temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
     }
 
     for (int i = 0; i < size; ++i)
